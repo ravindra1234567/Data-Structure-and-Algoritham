82418b1847d3a20153bfca903ff06fd6ec422b4f
ravindra1234567 <ravindrakushwahanwg@gmail.com>
2021-04-03 10:42:55 +0530

    add stack operation using linked list

---
+++ StackOperationUsingLinkedList.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+class Node{
+    int data;
+    Node next;
+}
+ class StackOperationUsingLinkedList {
+
+     Node front;
+
+    public Node createNode(int value){
+        
+        Node node = new Node();
+        node.data = value;
+        node.next = null;
+        return node; 
+    }
+
+   
+
+     public void push(int value){
+        
+        if(front == null){
+            front = createNode(value);
+            return;
+           
+        }
+        Node node = createNode(value);
+        node.next = front;
+        front = node;
+    }
+    public int pop(){
+        if(front == null){
+            System.out.println("Stack is Empty!");
+            return Integer.MIN_VALUE;
+        }else{
+        int temp = front.data;
+
+        front = front.next;
+
+        return temp;
+        }
+    }
+
+    void display(){
+        if(front == null){
+            System.out.println("Stack is Empty");
+        }else{
+            Node temp = front;
+            while(temp!=null){
+                System.out.println(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+    }
+
+    public static void main(String[] args){
+
+    StackOperationUsingLinkedList obj = new StackOperationUsingLinkedList();
+    obj.push(10);
+    obj.push(20);
+    obj.display();
+    // System.out.println(obj.pop());
+    // System.out.println(obj.pop());
+    // System.out.println(obj.pop());
+
+    }
+    
+}
